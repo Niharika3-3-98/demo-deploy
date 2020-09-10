e2b6a801eca10ce7b438f913da52362470d91c14
Rohan Panda <rohan.panda@ad.infosys.com>
2020-09-10 15:12:58 +0530

    commmit

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
+import { ItemsService} from './items.service'
 
 describe('Items Controller', () => {
   let controller: ItemsController;
+  let service : ItemsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
+      providers:[ItemsService],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
+    service = module.get<ItemsService>(ItemsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of items', async () => {
+      const result : any = ['test'];
+      jest.spyOn(ItemsService, 'findAll').mockImplementation(() => result);
+
+      expect(await ItemsController.findAll()).toBe(result);
+    });
   });
 });
